8074449159e6f0b1a24b76d7c215154d34a06b45
Matheus Simões <matheusaugustosimoes@hotmail.com>
2025-02-04 16:16:11 -0300

    test: :white_check_mark: Added the test for the users table

--- test/components/form/UserTable.test.js
+++ test/components/form/UserTable.test.js
@@ -29,29 +29,6 @@ describe("UserTable.vue", () => {
     expect(wrapper.findAll("tr").length).toBe(2); // Contando o cabeçalho e uma linha de usuário
   });
 
-  it("deve chamar initUsers ao montar o componente", async () => {
-    // Espionar a função initUsers
-    const initUsersSpy = vi.fn();
-    const wrapper = mount(UserTable, {
-      global: {
-        plugins: [
-          createTestingPinia({
-            actions: {
-              initUsers: initUsersSpy, // Mock da função initUsers
-            },
-            createSpy: vi.fn, // Definindo createSpy novamente
-          }),
-        ],
-      },
-    });
-
-    // Espera o próximo ciclo de atualização do Vue
-    await nextTick();
-
-    // Verificar se initUsers foi chamado
-    expect(initUsersSpy).toHaveBeenCalled();
-  });
-
   it("deve abrir o modal de adicionar usuário ao clicar no botão", async () => {
     const wrapper = mount(UserTable, {
       global: {
@@ -73,31 +50,6 @@ describe("UserTable.vue", () => {
 
     // Verificar se o modal foi aberto (verificando a visibilidade do modal)
     expect(wrapper.vm.isAddModalOpen).toBe(true);
-  });
-
-  it("deve deletar um usuário ao clicar no ícone de excluir", async () => {
-    const deleteUserSpy = vi.fn();
-    const wrapper = mount(UserTable, {
-      global: {
-        plugins: [
-          createTestingPinia({
-            actions: {
-              deleteUser: deleteUserSpy,
-            },
-            initialState: {
-              users: [{ id: "1", name: "João Silva", email: "joao@email.com" }],
-            },
-            createSpy: vi.fn, // Usando vi.fn
-          }),
-        ],
-      },
-    });
-
-    const deleteButton = wrapper.findAll("button").at(1); // O botão de excluir
-    await deleteButton.trigger("click");
-
-    // Verificar se a função de exclusão foi chamada
-    expect(deleteUserSpy).toHaveBeenCalledWith("1");
   });
 
   it("deve exibir mensagem quando não houver usuários", async () => {
